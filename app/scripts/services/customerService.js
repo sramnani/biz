8c6dd5640a554858cf851830b803f988c41ab831
sonia ramnani <sonia_ramnani@symantec.com>
2015-05-21 16:32:58 -0700

    modifued customer service

--- app/scripts/services/customerService.js
+++ app/scripts/services/customerService.js
@@ -1,21 +1,37 @@
 angular.module("outingzApp")
-        .factory("customerService", function ($http, $q, $window, $cookies) {
+    .factory("customerService", function ($http, $q, $window, $cookies) {
 
-            return {
-                add_customer: function (customerObj) {
-                    var deferred = $q.defer();
+        return {
+            addCustomer: function (customerObj) {
+                var deferred = $q.defer();
 
-                    //Calling Web API to add merchat data
-                    $http.post("https://api.outingz.com/outingz/subscriber", customerObj).success(function (data) {
-                        //Passing data to deferred's resolve function on successful completion
-                        deferred.resolve(data);
-                    }).error(function () {
+                //Calling Web API to add merchat data
+                $http.post("https://api.outingz.com/outingz/subscriber", customerObj).success(function (data) {
+                    //Passing data to deferred's resolve function on successful completion
+                    deferred.resolve(data);
+                }).error(function () {
 
-                        //Sending a friendly error message in case of failure
-                        deferred.reject("An error occured while fetching items");
-                    });
+                    //Sending a friendly error message in case of failure
+                    deferred.reject("An error occured while fetching items");
+                });
 
-                    return deferred.promise;
-                }
+                return deferred.promise;
+            },
+            getCustomers: function () {
+                var deferred = $q.defer();
+                // var key = $window.localStorage['keyy'];
+                var key = $cookies.keyy;
+                //Calling Web API to fetch merchant locations
+                $http.get("https://api.outingz.com/outingz/subscriber/"+key).success(function (data) {
+                    //Passing data to deferred's resolve function on successful completion
+                    deferred.resolve(data);
+                }).error(function () {
+
+                    //Sending a friendly error message in case of failure
+                    deferred.reject("An error occured while fetching items");
+                });
+
+                return deferred.promise;
             }
-        });
+        }
+    });
