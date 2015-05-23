93a5908e5376b290df1a54ba82e7b4fe60bb85a0
sramnani <sonia.r15@gmail.com>
2015-05-23 15:16:36 -0700

    added customer sort

--- app/scripts/controllers/customerCtrl.js
+++ app/scripts/controllers/customerCtrl.js
@@ -33,26 +33,22 @@ angular.module('outingzApp')
         ];
 
         $scope.tableParams = new ngTableParams({
-            page: 1,            // show first page
-            count: 3 // count per page
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                    $filter('orderBy')(data, params.orderBy()) :
+                    data;
 
-
-        }, {
-            total: data.length, // length of data
-            getData: function ($defer, params) {
-                // use build-in angular filter
-                 var orderedData = params.sorting() ?
-
-                                $filter('orderBy')(data, params.orderBy()) :
-
-                                data;
-                orderedData = params.filter ?
-                        $filter('filter')(orderedData, params.filter()) :
-                        orderedData;
-
-               $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));;
-            }
-        });
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
 
 
     }]);
