6db229ab6b09c7be8db95660457bfc675bf72b13
sramnani <sonia.r15@gmail.com>
2015-05-30 00:41:31 -0700

    added activity list

---
+++ app/scripts/controllers/activityListCtrl.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name outingzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the outingzApp
+ */
+angular.module('outingzApp')
+        .controller('activityListCtrl', function ($scope, activityService, $q, $http, $location, $window, $route,$timeout,$cookies,ngTableParams,$filter) {
+            console.log("class");
+
+
+        var data = [
+            {name: "Steel Bending", schedule: "9:30-11AM", location: "Mountain View", instructor: "Jerry"},
+             {name: "Rock Climbing", schedule: "5:30-8:30PM", location: "Palo alto", instructor: "John"}
+
+
+        ];
+
+        $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                    $filter('orderBy')(data, params.orderBy()) :
+                    data;
+
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
+
+
+
+        });
