90b237b52de0332b2a83ed2f645241d0ee483206
sramnani <sonia.r15@gmail.com>
2015-06-02 21:30:09 -0700

    added reservation controller

---
+++ app/scripts/controllers/activityReservationCtrl.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name outingzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the outingzApp
+ */
+angular.module('outingzApp')
+        .controller('activityReservationCtrl', function ($scope, activityService, $q, $http, $location, $window, $route,$timeout,$cookies) {
+            console.log("class");
+
+
+            //dummy list of US 50 states.
+
+
+
+        });
