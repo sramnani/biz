7189efcd9680416ab389145ed7bb3673178b91f0
sramnani <sonia.r15@gmail.com>
2015-06-02 20:54:48 -0700

    added dashboard and activity changes

--- app/scripts/controllers/editActivityCtrl.js
+++ app/scripts/controllers/editActivityCtrl.js
@@ -6,14 +6,55 @@
 
 
 angular.module('outingzApp')
+.run (function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
     .controller('editActivityCtrl', ['$scope', 'merchantService', '$q', '$http','$modal', '$routeParams', '$location', '$timeout', '$aside', 'userService', 'customerService', '$filter', '$rootScope', function ($scope, merchantService, $q, $http, $modal,$routeParams, $location, $timeout, $aside, userService, $rootScope, $modalInstance) {
 
+        $scope.offering = {};
+         $scope.user = {
+    id: 1,
+    name: 'awesome user',
 
+  };
         $scope.cancel = function () {
         $modal.close();
 
 
         };
+
+        $scope.saveUser = function (offering, isvalid) {
+
+                if (isvalid) {
+
+                    activityService.add_activity_class(offering).then(function (data) {
+
+                        $scope.success = "Activity Class added successfully";
+                        $timeout(function () {
+                            $scope.error = false;
+                            $scope.success = false;
+                            $scope.showValidation = false;
+                        }, 3000);
+
+                    }, function (error) {
+
+                        console.log("There is an error" + error);
+
+                    });
+
+                } else {
+
+                    $scope.showValidation = true;
+                    $scope.error = "Error in adding your class!";
+                    $timeout(function () {
+                        $scope.error = false;
+                        $scope.success = false;
+                        // $scope.showValidation = false;
+                    }, 3000);
+                }
+
+
+            };
 
 
     }]);
