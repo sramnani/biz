3de57fb24708f14f7bbe4cb1f13340dba7c1fc79
sramnani <sonia.r15@gmail.com>
2015-06-03 20:52:54 -0700

    fixed editActivity close button

--- app/scripts/controllers/editActivityCtrl.js
+++ app/scripts/controllers/editActivityCtrl.js
@@ -5,21 +5,24 @@
  */
 
 
-angular.module('outingzApp').controller('editActivityCtrl', ['$scope', 'merchantService', '$q', '$http','$modal', '$routeParams', '$location', '$timeout', '$aside', 'userService', 'customerService', '$filter', '$rootScope', function ($scope, merchantService, $q, $http, $modal,$routeParams, $location, $timeout, $aside, userService, $rootScope, $modalInstance) {
 
-        $scope.offering = {};
+angular.module('outingzApp')
+    .controller('editActivityCtrl', ['$scope', 'activityService', '$q', '$http', '$modalInstance','$modal', '$routeParams', '$location', '$timeout', '$aside', 'userService', 'customerService', '$filter', '$rootScope', function ($scope, activityService, $q, $http, $modal,$routeParams, $location, $timeout, $aside, userService, $rootScope, $modalInstance) {
 
+        $scope.service = {};
+        $scope.service.offering = {};
         $scope.cancel = function () {
-        $modal.close();
+                $modal.close();
+
+//        $modal.close();
 
 
         };
+        $scope.saveActivity = function (service) {
 
-        $scope.saveUser = function (offering, isvalid) {
 
-                if (isvalid) {
 
-                    activityService.add_activity_class(offering).then(function (data) {
+                    activityService.addActivityService(service).then(function (data) {
 
                         $scope.success = "Activity Class added successfully";
                         $timeout(function () {
@@ -34,20 +37,11 @@ angular.module('outingzApp').controller('editActivityCtrl', ['$scope', 'merchant
 
                     });
 
-                } else {
 
-                    $scope.showValidation = true;
-                    $scope.error = "Error in adding your class!";
-                    $timeout(function () {
-                        $scope.error = false;
-                        $scope.success = false;
-                        // $scope.showValidation = false;
-                    }, 3000);
-                }
 
 
             };
 
 
-    }]);
 
+    }]);
