a869db0cca229e2a6a4cb69b3c705762ce0b0056
sramnani <sonia.r15@gmail.com>
2015-06-04 17:57:26 -0700

    fixed issue with loader

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@
  */
 angular
         .module('outingzApp', [
-            'chieffancypants.loadingBar',
+            'ui.bootstrap',
             'ngAnimate',
             'ngCookies',
             'ngMessages',
@@ -19,7 +19,6 @@ angular
             'ngSanitize',
             'ngTouch',
             'ui.utils',
-            'ui.bootstrap',
             'ngAside',
             'ngCookies',
             'chart.js',
