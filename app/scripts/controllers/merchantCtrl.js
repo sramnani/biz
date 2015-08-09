bbcd3b50c349da6986a6a6d171333f728996deb4
sonia ramnani <sonia_ramnani@symantec.com>
2015-08-09 12:38:51 -0700

    added location tags

--- app/scripts/controllers/merchantCtrl.js
+++ app/scripts/controllers/merchantCtrl.js
@@ -17,6 +17,8 @@ angular.module('outingzApp')
             $scope.image_social = "images/social_img.png";
             $scope.image_payment = "images/payment_img.png";
             $scope.image_staff = "images/staff_img.png";
+            $scope.vm = {};
+            $scope.vm.address = {};
 
             $scope.msg = false;
 
@@ -42,40 +44,14 @@ angular.module('outingzApp')
             $scope.loader=false;
             $scope.loader_img="images/ajax-loader.gif";
             
-            if ($location.url() === "/merchant" || $location.url() === "/location") {
-                $scope.loader=true;
-                merchantService.get_merchat().then(function (data) {
-                    $scope.loader=false;
-                    $scope.merchant = data;
-                    if($location.url() === "/location"){
-                        $scope.primaryLine = $scope.merchant.primaryAddress.line1;
-                        $scope.location.locationName = $scope.merchant.primaryAddress.line2;
-                        $scope.location.address.line1 = $scope.merchant.primaryAddress.line1;
-                        $scope.location.address.line2 = $scope.merchant.primaryAddress.line2;
-                        $scope.location.address.state = $scope.merchant.primaryAddress.state;
-                        $scope.location.address.zip = $scope.merchant.primaryAddress.zip;
-                        $scope.location.address.city = $scope.merchant.primaryAddress.city;
-                        $scope.location.contactPerson={};
-                        $scope.location.contactPerson.name={};
-                        $scope.location.contactPerson.phone={};
-                        $scope.location.contactPerson.emailId={};
-                        $scope.location.contactPerson.name.firstName = $scope.merchant.primaryContactPerson.name.firstName;
-                        $scope.location.contactPerson.phone.number = $scope.merchant.primaryContactPerson.phone.number;
-                        $scope.location.contactPerson.emailId = $scope.merchant.primaryContactPerson.emailId;
-                    }                    
-                    // $scope.merchant.description=data.description;
-                }, function (error) {
-                    $scope.error = "Error in creating your bussiness!";
-                });
-            }
-            
+
             $scope.link_it = function(){
                 $location.url('/activity_service');
             }
 
             // Used to setup the business for first time (Creates merchant)
             $scope.create_merchant = function (merchant, isvalid) {
-
+                isvalid = true;
                 if (isvalid) {
 
                     // $scope.location.address.country = "USA";
@@ -83,6 +59,20 @@ angular.module('outingzApp')
 
                     merchant.locations = {};
                     merchant.locations.location = [];
+                    if($scope.location){
+                        if($scope.location.address.street_number){
+                            $scope.location.address.line1 = $scope.location.address.street_number;
+                        }
+                        if($scope.location.address.route){
+                            $scope.location.address.line2 = $scope.location.address.route;
+                        }
+                        if($scope.location.address.locality){
+                            $scope.location.address.city = $scope.location.address.locality;
+                        }
+                        if($scope.location.address.administrative_area_level_1){
+                            $scope.location.address.state = $scope.location.address.administrative_area_level_1;
+                        }
+                    }
 
                     merchant.locations.location.push($scope.location);
                     // var key = $window.localStorage['keyy'];
@@ -93,21 +83,11 @@ angular.module('outingzApp')
                     merchantService.add_merchant(merchant).then(function (data) {
                         $scope.success = "Your Business setup sucessfully";
 
-                        $timeout(function () {
-                            $location.url('/location');
-                        }, 2000);
+
                     }, function (error) {
                         $scope.error = "Error in creating your bussiness!";
                     });
 
-                } else {
-
-                    $scope.showValidation = true;
-                    $scope.error = "Error in creating your bussiness!";
-                    $timeout(function () {
-                        $scope.error = false;
-                        $scope.success = false;
-                    }, 3000);
                 }
             }
 
@@ -118,48 +98,7 @@ angular.module('outingzApp')
             $scope.location.locationSchedules.locationSchedules  = [];
             
             // Used to add locations for the merchant.
-            $scope.add_merchant_location = function (locations, isvalid) {
 
-
-                if (isvalid) {
-                    
-                    locations.address.country = "Secondary";
-                    //locations.address.line2 = "";
-
-                    angular.forEach($scope.locationSchedule, function (value, key) {
-                        $scope.location.locationSchedules.locationSchedules.push(value);
-                    });
-                   console.log(JSON.stringify($scope.location));
-
-                    merchantService.add_merchant_location(locations).then(function (data) {
-                        $scope.locations_data.push(locations);
-                        $scope.location = {};
-                        $scope.location.address = {};
-
-                        $scope.success = "Your location added sucessfully";
-                    }, function (error) {
-                        $scope.error = "Error in saving your location!";
-                    });
-                } else {
-                    $scope.showValidation = true;
-                    $scope.error = "Error in Adding your location!";
-                    $timeout(function () {
-                        $scope.error = false;
-                        $scope.success = false;
-                    }, 3000);
-                }
-
-            }
-
-
-            //get locations when url is /location	
-            if ($location.url() === "/location") {
-                merchantService.get_merchant_locations().then(function (data) {
-                    $scope.locations_data = data.location;
-                }, function (error) {
-                    console.log("There is an error" + error);
-                });
-            }
 
 
         });
