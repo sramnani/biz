f41ebcdec4d3ce514754cc2ec71c07180d217bee
sonia ramnani <sonia_ramnani@symantec.com>
2015-08-19 16:43:31 -0700

    added service form

--- app/scripts/controllers/editActivityCtrl.js
+++ app/scripts/controllers/editActivityCtrl.js
@@ -5,61 +5,78 @@
  */
 
 
-
 angular.module('outingzApp')
-    .controller('editActivityCtrl', ['$scope', 'activityService', '$q', '$http', '$modalInstance','$modal', '$routeParams', '$location', '$timeout', '$aside', 'userService', 'customerService', '$filter', '$rootScope', function ($scope, activityService, $q, $http, $modal,$routeParams, $location, $timeout, $aside, userService, $rootScope, $modalInstance) {
+    .controller('editActivityCtrl', ['$scope', 'activityService', '$q', '$http', '$modalInstance', '$modal', '$routeParams', '$location', '$timeout', '$aside', 'userService', 'customerService', '$filter', '$rootScope', function ($scope, activityService, $q, $http, $modal, $routeParams, $location, $timeout, $aside, userService, $rootScope, $modalInstance) {
 
         $scope.service = {};
         $scope.service.offering = {};
-$scope.vm = {
-      address: {}
-    };
-    $scope.details = {};
+        $scope.service.activity = {};
+
+
+        $scope.vm = {
+            address: {}
+        };
+        $scope.details = {};
 
 
         $scope.cancel = function () {
-                $modal.close();
-                 $scope.options = null;
-    $scope.details = '';
+            $modal.close();
+            $scope.options = null;
+            $scope.details = '';
 
 //        $modal.close();
 
 
         };
-        $scope.reload = function(){
-        if($scope.params){
-        $scope.activityName = $scope.params.activityName;
-        $scope.service.offering.location = $scope.params.location;
-        $scope.service.offering.description = $scope.params.description;
-        $scope.service.offering.instructor = $scope.params.instructor;
-        }
-                    $scope.result1 = 'initial value';
+        $scope.reload = function () {
+            console.log($scope.param);
+            if ($scope.param) {
+                $scope.service.serviceId = $scope.param.id;
+                $scope.service.activity.name = $scope.param.activity.name;
+                $scope.service.activity.description = $scope.param.activity.description;
+                $scope.service.instructor = $scope.param.instructors.instructor[0].name.firstName;
+
+            }
+            $scope.result1 = 'initial value';
 
         };
 
         $scope.saveActivity = function (service) {
+            $scope.service.instructors = {};
+            $scope.service.instructors.instructor = [];
+            $scope.service.instructors.instructor.push($scope.service.instructor);
 
 
+            activityService.add_activity_service(service).then(function (data) {
 
-                    activityService.addActivityService(service).then(function (data) {
-
-                        $scope.success = "Activity Class added successfully";
-                        $timeout(function () {
-                            $scope.error = false;
-                            $scope.success = false;
-                            $scope.showValidation = false;
-                        }, 3000);
-
-                    }, function (error) {
-
-                        console.log("There is an error" + error);
-
-                    });
+                $scope.success = "Activity Class added successfully";
 
 
+            }, function (error) {
+
+                console.log("There is an error" + error);
+
+            });
 
 
-            };
+        };
+        $scope.updateActivity = function (service,serviceId) {
+
+
+            $scope.service.id = serviceId;
+            activityService.update_activity_service(service,serviceId).then(function (data) {
+
+                $scope.success = "Activity Class updated successfully";
+
+
+            }, function (error) {
+
+                console.log("There is an error" + error);
+
+            });
+
+
+        };
         $scope.reload();
 
 
