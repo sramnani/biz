020d601732b8e20db30a63674d32b2bb71d5c26b
sonia ramnani <sonia_ramnani@symantec.com>
2015-08-19 16:44:18 -0700

    connect update API's from the UI

--- app/scripts/services/activityService.js
+++ app/scripts/services/activityService.js
@@ -2,7 +2,50 @@ angular.module("outingzApp")
 .factory("activityService", function ($http,$q,$window,$cookies) {
 	
 	return {
+		add_activity : function(activity){
 
+
+			var deferred = $q.defer();
+
+			var link="";
+			//var key = $window.localStorage['keyy'];
+			var key = $cookies.keyy;
+			link = "http://localhost:8080/outingz/merchants/"+key+"/activities";
+
+			//Calling Web API to fetch health data
+			$http.post(link,activity).success(function(data){
+				//Passing data to deferred's resolve function on successful completion
+				deferred.resolve(data);
+			}).error(function(){
+
+				//Sending a friendly error message in case of failure
+				deferred.reject("An error occured while fetching items");
+			});
+
+			return deferred.promise;
+		},
+		getActivities : function(){
+
+
+			var deferred = $q.defer();
+
+			var link="";
+			//var key = $window.localStorage['keyy'];
+			var key = $cookies.keyy;
+			link = "http://localhost:8080/outingz/merchants/"+key+"/activities/dashboard";
+
+			//Calling Web API to fetch health data
+			$http.get(link).success(function(data){
+				//Passing data to deferred's resolve function on successful completion
+				deferred.resolve(data);
+			}).error(function(){
+
+				//Sending a friendly error message in case of failure
+				deferred.reject("An error occured while fetching items");
+			});
+
+			return deferred.promise;
+		},
 		add_activity_service : function(activity_service){
 			
 			
@@ -11,7 +54,7 @@ angular.module("outingzApp")
 			var link="";
 			//var key = $window.localStorage['keyy'];
 			var key = $cookies.keyy;
-                        link = "https://api.outingz.com/outingz/merchants/"+key+"/services";			
+                        link = "http://localhost:8080/outingz/merchants/"+key+"/services";
 
 			//Calling Web API to fetch health data
 			$http.post(link,activity_service).success(function(data){
@@ -25,7 +68,28 @@ angular.module("outingzApp")
 			
 			return deferred.promise; 
 		},
+		update_activity_service : function(activity_service,serviceId){
 
+
+			var deferred = $q.defer();
+
+			var link="";
+			//var key = $window.localStorage['keyy'];
+			var key = $cookies.keyy;
+			link = "http://localhost:8080/outingz/merchants/"+key+"/services/"+serviceId;
+
+			//Calling Web API to fetch health data
+			$http.put(link,activity_service).success(function(data){
+				//Passing data to deferred's resolve function on successful completion
+				deferred.resolve(data);
+			}).error(function(){
+
+				//Sending a friendly error message in case of failure
+				deferred.reject("An error occured while fetching items");
+			});
+
+			return deferred.promise;
+		},
 
 
 
